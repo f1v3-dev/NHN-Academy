0f82a0533d64f4bc77b0fa7af541cb5d7ed1d316
f1v3-dev <f1v3@kakao.com>
2023-08-29 17:39:43 +0900

    윤년 판별 프로그램

--- NHN/NHN2023_08_29/20230829/Demonstrate.java
+++ NHN/NHN2023_08_29/20230829/Demonstrate.java
@@ -1,16 +1,52 @@
 import java.util.Scanner;
 
 public class Demonstrate {
+
+    public static boolean calcYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        else return false;
+    }
+
     public static void main(String[] args) {
+
+        // 윤년 : leaf year : 4로 나누어 떨어지는 년도, 100으로 나누어 떨어지나, 400으로 나누어 떨어지면 윤년
+        // 평년 : common year : 4로 나누어 떨어지더라도 100으로 떨어지면 평년
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a digit: ");
-        int f = scanner.nextInt();
-        float c = (f - 32) * 5.0f / 9.0f;
+        System.out.print("년도를 입력하세요 : ");
+        int year = scanner.nextInt();
 
-        System.out.println("입력하신 " + f + "°F는 " + c + "°C입니다.");
+        // if (year % 4 == 0) {
+        //     if (year % 100 == 0) {
+        //         if (year % 400 == 0) {
+        //             System.out.println(year + "년은 윤년입니다.");
+        //             return;
+        //         }
+        //         System.out.println(year + "년은 평년입니다.");
+        //         return;
+        //     }
+        //     System.out.println(year + "년은 윤년입니다.");
+        // }
 
-        scanner.close();
+        // else {
+        //     System.out.println(year + "년은 평년입니다.");
+        // }
+
+        boolean isLeaf = calcYear(year);
+        if (isLeaf) {
+            System.out.println(year + "년은 윤년입니다.");
+        } else {
+            System.out.println(year + "년은 평년입니다.");
+        }
 
     }
 }
