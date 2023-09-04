874fb9f1860233532ebc6c29d7e0eb89b267b5d6
f1v3-dev <f1v3@kakao.com>
2023-09-04 17:01:04 +0900

    compare Code

--- NHN/NHN2023_09_04/Lab4-1/Starter/Whatday.java
+++ NHN/NHN2023_09_04/Lab4-1/Starter/Whatday.java
@@ -1,98 +1,66 @@
 import java.util.Scanner;
 
 public class Whatday {
-    static int[] daysInMonth  = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    static String[] monthNames = {"January", "Feburary", "March", "April", "May", "June", "July", "August", "Sepetember", "October", "November", "December"};
-    
+    static int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static int[] daysInLeapMonth = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    static String[] monthNames = { "January", "Feburary", "March", "April", "May", "June", "July", "August",
+            "Sepetember", "October", "November", "December" };
+
     public static void main(String[] args) {
-        //
-        // To do: add code here
-        // 
 
-        /* calculate Month Number and days in Month using if statement
-        if (dayNum >= 31) {     // January
-            monthNum++;
-            dayNum -= 31;
+        try {
+            System.out.print("Enter year number: ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            int yearNum = Integer.parseInt(line);
+
+            boolean isLeapYear = (yearNum % 4 == 0) && (yearNum % 100 != 0 || yearNum % 400 == 0);
+
+            if (isLeapYear) {
+                
+                System.out.println("it's Leaf Year");
+            }
+            else {
+                System.out.println("it's Common Year");
+            }
+            
+            int maxDayNum = isLeapYear ? 366 : 365;
+
+            System.out.print("Enter a digit 1 between " + maxDayNum + " : ");
+            line = scanner.nextLine();
+            int dayNum = Integer.parseInt(line);
+
+            if (dayNum <= 0 || dayNum > maxDayNum) {
+                throw new IllegalArgumentException("Out Of Date");
+            }
+
+            int monthNum = 0;
+            if (!isLeapYear) {
+                for (int days : daysInMonth) {
+                    if (dayNum <= days) {
+                        break;
+                    } else {
+                        dayNum -= days;
+                        monthNum++;
+                    }
+                }
+            }
+            else {
+                for (int days : daysInLeapMonth) {
+                    if (dayNum <= days) {
+                        break;
+                    } else {
+                        dayNum -= days;
+                        monthNum++;
+                    }
+                }
+            }
+            
+            String monthName = monthNames[monthNum];
+
+            System.out.println(monthName + " " + dayNum);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
         }
-
-        if (dayNum >= 28) {     // Feburary
-            monthNum++;
-            dayNum -= 28;
-        }
-
-        if (dayNum >= 31) {     // March
-            monthNum++;
-            dayNum -= 31;
-        }
-
-        if (dayNum >= 30) {     // April
-            monthNum++;
-            dayNum -= 30;
-        }
-
-        if (dayNum >= 31) {     // May
-            monthNum++;
-            dayNum -= 31;
-        }
-
-        if (dayNum >= 30) {     // June
-            monthNum++;
-            dayNum -= 30;
-        }
-
-        if (dayNum >= 31) {     // July
-            monthNum++;
-            dayNum -= 31;
-        }
-
-        if (dayNum >= 31) {     // August
-            monthNum++;
-            dayNum -= 31;
-        }
-
-        if (dayNum >= 30) {     // September
-            monthNum++;
-            dayNum -= 30;
-        }
-
-        if (dayNum >= 31) {     // October
-            monthNum++;
-            dayNum -= 31;
-        } */
-
-
-       /*  Assign month name using switch statement
-       switch(monthNum) {
-            case 0:
-                monthName = "January";
-                break;
-            case 1:
-                monthName = "Feburary";
-                break;
-            case 2:
-                monthName = "March";
-                break;
-            case 3:
-                monthName = "April";
-                break;
-            case 4:
-                monthName = "May";
-                break;
-            case 5:
-                monthName = "June";
-                break;
-            case 6:
-                monthName = "July";
-                break;
-            case 7:
-                monthName = "August";
-                break;
-            case 8:
-                monthName = "September";
-                break;
-            case 9:
-                monthName = "October";
-                break;
-        } */
     }
 }
