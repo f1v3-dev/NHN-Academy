d419a9cfa0ab83c303bb43676e7a01d41659b520
f1v3-dev <f1v3@kakao.com>
2023-09-04 20:45:35 +0900

    틱택토 게임 구현

--- NHN/NHN2023_09_04/Lab4-1/Starter/Sample.java
+++ NHN/NHN2023_09_04/Lab4-1/Starter/Sample.java
@@ -1,45 +1,16 @@
 public class Sample {
 
-    // 정수형 2개를 받은 후 같으면 true, 아니면 false
-    static boolean compare(int i, int j) {
-        if (i == j) {
-            return true;
-        }else {
-            return false;
-        }
+    static int add(int i, int j) {
+        return i + j;
     }
 
+    static int add(int i, double d) {
+        return i + (int) d;
+    }
     public static void main(String[] args) {
-        int i = 1;
-        int k = 2;
-
-        if (compare(i, k)) {
-            System.out.println("Escape");
-        }
-
-        if (i == k) {
-            System.out.println("escape");
-        } else {
-            System.out.println("Stay");
-        }
-
-        i = 5;
-        k = 9;
-
-        if (i == k) {
-            System.out.println("Escape");
-        } else {
-            System.out.println("Stay");
-        }
-
-        i = 5;
-        k = 10;
-
-        if (i == k) {
-            System.out.println("Escape");
-        } else {
-            System.out.println("Stay");
-        }
+        double d = 5.0d;
+        int result = add(1, d);
+        result = add(2, 1);
 
     }
 }
---
+++ NHN/NHN2023_09_04/Lab4-1/Starter/Tictactoe.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+public class Tictactoe {
+    
+    static int count = 0;
+
+    static char[][] map = new char[3][3];
+
+    static void printMap() {
+        for (int i = 0; i < 2; i++) {
+            System.out.printf(" %c |  %c  | %c \n", map[i][0], map[i][1], map[i][2]);
+            System.out.println("- - - - - - -");
+        }
+        System.out.printf(" %c |  %c  | %c \n", map[2][0], map[2][1], map[2][2]);
+        System.out.println();
+    }
+
+    static void fillMap(int x, int y, boolean isPlayer1) {
+
+        if (map[x][y] != ' ') {
+            throw new IllegalArgumentException("this position is not empty!!!");
+        }
+
+        if (isPlayer1) {
+            map[x][y] = 'X';
+        } else {
+            map[x][y] = 'O';
+        }
+    }
+
+    public static boolean isEnd(char[][] map) {
+
+        count++;
+        if (count > 9) {
+            System.out.println("Draw...");
+            return true;
+        }
+
+        for (int i = 0; i < map.length; i++) {
+            if ((map[i][0] == map[i][1] && map[i][1] == map[i][2]) && map[i][0] != ' ') {
+                if (map[i][0] == 'X') {
+                    System.out.println("[Player1] Win !!");
+                    return true;
+                } else {
+                    System.out.println("[Player2] Win !!");
+                    return true;
+                }
+            } 
+            else if ((map[0][i] == map[1][i] && map[1][i] == map[2][i]) && map[0][i] != ' ') {
+                if (map[0][i] == 'X') {
+                    System.out.println("[Player1] Win !!");
+                    return true;
+                } else {
+                    System.out.println("[Player2] Win !!");
+                    return true;
+                }
+            }
+        }
+
+        if ((map[0][0] == map[1][1] && map[1][1] == map[2][2]) && map[0][0] != ' ') {
+            if (map[0][0] == 'X') {
+                System.out.println("[Player1] Win !!");
+                return true;
+            } else {
+                System.out.println("[Player2] Win !!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = ' ';
+            }
+        }
+
+        String[] input;
+        boolean isPlayer1 = true;
+        while (true) {
+            printMap();
+            if (isEnd(map)) {
+                break;
+            }
+
+            System.out.print("[Player 1] Enter the position (x y): ");
+            input = scanner.nextLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+
+            if ((x < 0 || 2 < x) || (y < 0 || 2 < y)) {
+                throw new IllegalArgumentException();
+            }
+            fillMap(x, y, isPlayer1);
+            printMap();
+            if (isEnd(map)) {
+                break;
+            }
+
+            isPlayer1 = false;
+            System.out.print("[Player 2] Enter the position (x y): ");
+            input = scanner.nextLine().split(" ");
+            x = Integer.parseInt(input[0]);
+            y = Integer.parseInt(input[1]);
+            if ((x < 0 || 2 < x) || (y < 0 || 2 < y)) {
+                throw new IllegalArgumentException();
+            }
+            fillMap(x, y, isPlayer1);
+            isPlayer1 = true;
+
+        }
+    }
+}
