7a98237fd8d34ac6f8594c06dad7796067f94a32
f1v3-dev <f1v3@kakao.com>
2023-09-05 16:47:50 +0900

    BankAccount Code 작성

--- NHN/NHN2023_09_05/AccountNumbers/CreateAccount/Starter/BankAccount.java
+++ NHN/NHN2023_09_05/AccountNumbers/CreateAccount/Starter/BankAccount.java
@@ -1,38 +1,57 @@
 import java.math.*;
 
 public class BankAccount {
-    public long accountNumber;
-    public String ownerName;
-    public BigDecimal balance;
+    private long accountNumber;
+    private String ownerName;
+    private BigDecimal balance;
 
-    //
-    // add setData method here.
-    //
+    private static long nextAccountNumber;
 
-    //
-    // add getNumber method here.
-    //
+    private static long nextNumber() {
+        return nextAccountNumber++;
+    }
+
+    public void setData(String ownerName, BigDecimal balance) {
+        this.accountNumber = nextNumber();
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public long getNumber() {
+        return this.accountNumber;
+    }
+
+    public String getName() {
+        return this.ownerName;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
 }
 
 class CreateAccount {
-    public static BankAccount createNewBankAccount(long accountNumber, String ownerName, BigDecimal balance) {
-        BankAccount newAccount;
-        
-        newAccount.accountNumber = accountNumber;
-        newAccount.ownerName = ownerName;
-        newAccount.balance = balance;
+    public static BankAccount createNewBankAccount(String ownerName, BigDecimal balance) {
+        BankAccount newAccount = new BankAccount();
+
+        newAccount.setData(ownerName, balance);
 
         return newAccount;
     }
 
-    //public static void main(String[] args) {
-    //    BankAccount bankAccount = CreateAccount.createNewBankAccount(1, "Vesper Lind", new BigDecimal("0.0"));
-    //    printBankAccount(bankAccount);
-    //}
+    public static void main(String[] args) {
+        BankAccount bankAccount1 = CreateAccount.createNewBankAccount("Vesper Lind", new BigDecimal("0.0"));
+        printBankAccount(bankAccount1);
+
+        System.out.println();
+
+        BankAccount bankAccount2 = CreateAccount.createNewBankAccount("James Bond", new BigDecimal("0.0"));
+        printBankAccount(bankAccount2);
+    }
 
     public static void printBankAccount(BankAccount account) {
-        System.out.println("Account Number: " + account.accountNumber);
-        System.out.println("Owner Name: " + account.ownerName);
-        System.out.println("Balance: " + account.balance);
+        System.out.println("Account Number: " + account.getNumber());
+        System.out.println("Owner Name: " + account.getName());
+        System.out.println("Balance: " + account.getBalance());
     }
 }
